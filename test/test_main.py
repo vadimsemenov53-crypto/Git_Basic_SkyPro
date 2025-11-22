6ea4ec18f0f05e83b746a77b11a4ac8b58299421
Vadim Semenov <vadimsemenov53@gmail.com>
2025-11-22 17:34:38 +0300

    fixes for develop

--- test/test_main.py
+++ test/test_main.py
@@ -7,3 +7,5 @@ save_file("russian_names.txt", "\n".join(filtered_name_rus))
 
 filtered_name_eng = filter_english_names(cleared_names)
 save_file("english_names.txt", "\n".join(filtered_name_eng))
+
+# Изменения для GitFlow
